57cc2255c7ad62c0e061c24b9de92ca0338df578
Francesco <francesco291104@gmail.com>
2024-07-14 16:40:16 +0200

    Fix semplici

--- main.c
+++ main.c
@@ -1,21 +1,23 @@
 #include "header/sudoku.h"
 #include "header/io.h"
 #include <stdlib.h>
+#include <stdbool.h>
 
-int main() {
+int main(int argc, char* argv[]) {
     int matrix[N][N];
     const char* inputFilename = "./sudoku_input.txt";
     const char* outputFilename = "./sudoku_output.txt";
-
+    bool print = false;
     if (!readSudoku(inputFilename, matrix)) {
         fprintf(stderr, "Failed to read Sudoku from file.\n");
         return EXIT_FAILURE;
     }
-
     printSudoku(matrix);
-    printf("\nSolving...\n");
+    printf("\nSolving...\n \n");
     if (solveSudoku(matrix)) {
-        printSudoku(matrix);
+        if(print == true){
+            printSudoku(matrix);
+        }
         if (!writeSudoku(outputFilename, matrix)) {
             fprintf(stderr, "Failed to write Sudoku to file.\n");
             return EXIT_FAILURE;
@@ -23,6 +25,6 @@ int main() {
     } else {
         printf("No solution exists\n");
     }
-
+    printf("File 'sudoku_output.txt' saved successfully!\n");
     return EXIT_SUCCESS;
 }
